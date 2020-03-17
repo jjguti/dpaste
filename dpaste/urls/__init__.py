9cb633e597947f5dd0728841b768b830ae360d39
Juan José Gutiérrez de Quevedo Pérez <juanjo@gutierrezdequevedo.com>
2020-03-17 14:40:17 +0100

    add the posibility of specifying a url prefix
    this commit implements a prefix for urls, which allows easily deploying
    dpaste into a "subdirectory". It adds a url_prefix to be read by default
    from the environment which makes it especially suitable for using for
    docker containers. For example, running
    
    docker run -e PREFIX_URL=dpaste/ -p 8000:8000 barttc/dpaste
    
    will make dpaste available on
    
    http://localhost:8000/dpaste/
    
    instead of the default http://localhost:8000
    
    This is specially useful if you want to proxy dpaste from another host
    and let it live in a sub-url. Without this commit all urls would be
    incorrectly generated in this case.

--- dpaste/urls/__init__.py
+++ dpaste/urls/__init__.py
@@ -1,9 +1,12 @@
+from django.conf import settings
 from django.conf.urls import include, url
 
+url_prefix = getattr(settings, "URL_PREFIX", "")
+
 urlpatterns = [
-    url(r"^", include("dpaste.urls.dpaste_api")),
-    url(r"^", include("dpaste.urls.dpaste")),
-    url(r"^i18n/", include("django.conf.urls.i18n")),
+    url(r"^%s" % url_prefix, include("dpaste.urls.dpaste_api")),
+    url(r"^%s" % url_prefix, include("dpaste.urls.dpaste")),
+    url(r"^%si18n/" % url_prefix, include("django.conf.urls.i18n")),
 ]
 
 # Custom error handlers which load `dpaste/<code>.html` instead of `<code>.html`
